7f201cecd4a269e91f7d8cdf2c840d9454fb16c0
IiljaTrpkovski <ilija.trpkovski@gmail.com>
2018-02-19 22:02:15 +0100

    cas1

---
+++ c1/index.js
@@ -0,0 +1,85 @@
+// console.log('Hello World');
+
+// var string = "Test test test";
+// var string2 = 'Test2 test2 test2';
+// var string3 = `Test 3 test3 test3`;
+
+// console.log(string);
+// console.log(string2);
+// console.log(string3);
+
+// var integer = 12;
+
+// console.log(integer);
+
+// var float = 3.14;
+// console.log(float);
+
+// var booleanT = true;
+// var booleanF = false;
+
+// console.log(booleanT);
+// console.log(booleanF);
+
+// var niza1 = ['pero', 'janko', 'stanko'];
+// console.log(niza1);
+// console.log(niza1[0]);
+// console.log(niza1[1]);
+// console.log(niza1[2]);
+
+// // niza1[100] = 'Ilija';
+// niza1.push('Ilija');
+
+// console.log(niza1.length);
+
+// for (let i = 0; i<niza1.length; i++ )
+// {
+//     console.log(niza1[i]);l 
+// }
+
+// for(let i in niza1)
+// {
+//     console.log(niza1[i]);
+//     break;
+// }
+
+
+// var broevi = [4,3, 5, 9, 15];
+
+// for(let i=0; i < broj.length; i++){
+//     if (broj[i]%15 == 0){
+//         console.log('fizbuzz')
+//     }else if (broj[i]%5 == 0){
+//         console.log('buzz')
+//     }else if (broj[i]%3){
+//         console.log('fiz')
+//     }else {
+//         console.log('ne e deliv ni so 3 ni so 5')
+//     }
+// }
+
+// for(let i in broevi){
+//     var o='';
+
+//     if(broevi[i] % 3 == 0){
+//         o += 'fizz';
+//     }
+//     if(broevi[i] % 5 == 0){
+//         o =+ 'buzz';
+//     }
+
+//     console.log(broevi[i] = "\t = \t" + o);
+// }
+
+
+var brojac = 0;
+
+while(brojac <= 10){
+    console.log(brojac);
+    brojac++;
+}
+
+do {
+    console.log(brojac);
+    brojac++
+}while (brojac < 10);
