d6873edb8e242b5e1ad95b1fea6c4cb853f64c3f
IiljaTrpkovski <ilija.trpkovski@gmail.com>
2018-02-26 22:00:03 +0100

    c3

---
+++ c3/index.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+
+var http = require("http");
+            
+// fs.writeFile('poraka.txt', 'НодеЈС е најдобар јазик на свет... и пошироко...',"utf8", (err) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
+
+// fs.appendFile('poraka.txt', 'i taka nekako\n', (err) => {
+//     if (err) throw err;
+//     console.log('The "data to append" was appended to file!');
+//   });
+
+//   fs.readFile('user.json','utf8', (err, data) => {
+//     // console.log(data);
+//     var jsonData = JSON.parse(data);
+//     // console.log(jsonData);
+//     jsonData.forEach((user, i) => {
+//         console.log('Name:' + user.name + '\t' + 'Lastname: ' + user.lastname + '\t' + 'Email:' + user.email + '\t' + 'Password' + user.password);
+//     })
+//   })
+
+http.createServer(function(request, response) { // req, res | r, w
+    // console.log(request);
+    // response.writeHead(200,'OK');
+    // response.end('Hello World!');
+
+    if(request.url === '/hello'){
+        response.writeHead(200,'OK');
+        response.end('Hello World!');
+    } else {
+        response.writeHead(404,'Not Found');
+        response.end('Page not found...');
+    }
+}).listen(3000);
+
